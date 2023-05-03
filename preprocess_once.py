817b1617ae58ca6ef31f9995a261e8859ce55208
MortonPL <MortonPL@users.noreply.github.com>
2023-05-03 18:29:31 +0200

    Add image preprocessing

--- preprocess_once.py
+++ preprocess_once.py
@@ -1,6 +1,8 @@
-import xml.etree.ElementTree as ET
 import os
-import shutil
+from xml.etree.ElementTree import parse as xml_parse
+from shutil import make_archive, unpack_archive
+from typing import List
+from PIL import Image
 
 classLUT = {
     'aircraft': '0',
@@ -17,8 +19,9 @@ classLUT = {
     'holicopter': '4',              # typo in the dataset!!!
 }
 
-def process_xml(filename):
-    root = ET.parse(filename).getroot()
+
+def process_xml(filename: str) -> List[List[str]]:
+    root = xml_parse(filename).getroot()
     size = root.find('size')
     boxes = [[size.find('width').text, size.find('height').text]]
     for child in root.findall('object'):
@@ -27,29 +30,65 @@ def process_xml(filename):
         boxes.append([classLUT[name.text], bndbox.find('xmin').text, bndbox.find('ymin').text, bndbox.find('xmax').text, bndbox.find('ymax').text])
     return boxes
 
-def process_annotations(filename, outdir):
+
+def process_annotations(filename: str, outdir: str) -> None:
     boxes = process_xml(filename)
     name = os.path.basename(filename)
     name, _ = os.path.splitext(name)
     with open(os.path.join(outdir, name + '.csv'), 'w') as out:
         out.writelines([','.join(box) + '\n' for box in boxes])
 
+
+def process_images(filename: str, outdir: str) -> None:
+    IMG_SIDE = 416
+    BLACK = (0, 0, 0)
+    name = os.path.basename(filename)
+    img = Image.open(filename)
+    ratio = img.width / img.height
+    if ratio > 1:
+        new_size = int(IMG_SIDE), int(IMG_SIDE / ratio)
+    else:
+        new_size = int(IMG_SIDE * ratio), int(IMG_SIDE)
+    img = img.resize(new_size, Image.Resampling.LANCZOS)
+    new = Image.new(img.mode, (IMG_SIDE, IMG_SIDE), BLACK)
+    new.paste(img, (0, 0))
+    new.save(os.path.join(outdir, name))
+
+
 if __name__ == "__main__":
-    shutil.unpack_archive('train-MADAI.zip', 'train-MADAI', format='zip')
-    shutil.unpack_archive('test-MADAI.zip', 'test-MADAI', format='zip')
+    TRAIN_ZIP_IN = 'train-MADAI'
+    TRAIN_ZIP_OUT = 'train-new'
+    TRAIN_CSV_IN = os.path.join(TRAIN_ZIP_IN, 'Train_Annotations')
+    TRAIN_CSV_OUT = os.path.join(TRAIN_ZIP_OUT, 'annotations')
+    TRAIN_IMG_IN = os.path.join(TRAIN_ZIP_IN, 'Train_JPEGImages')
+    TRAIN_IMG_OUT = os.path.join(TRAIN_ZIP_OUT, 'images')
 
-    inpath = 'train-MADAI/Train_Annotations'
-    outpath = 'train/annotations'
-    os.makedirs(outpath, exist_ok=True),
-    for file in os.listdir(inpath):
-        process_annotations(os.path.join(inpath, file), outpath)
+    TEST_ZIP_IN = 'test-MADAI'
+    TEST_ZIP_OUT = 'test-new'
+    TEST_CSV_IN = os.path.join(TEST_ZIP_IN, 'Annotations')
+    TEST_CSV_OUT = os.path.join(TEST_ZIP_OUT, 'annotations')
+    TEST_IMG_IN = os.path.join(TEST_ZIP_IN, '.')
+    TEST_IMG_OUT = os.path.join(TEST_ZIP_OUT, 'images')
 
-    inpath = 'test-MADAI/Annotations'
-    outpath = 'test/annotations'
-    os.makedirs(outpath, exist_ok=True),
-    for dir in os.listdir(inpath):
-        for file in os.listdir(os.path.join(inpath, dir)):
-            process_annotations(os.path.join(inpath, dir, file), outpath)
+    unpack_archive(TRAIN_ZIP_IN + '.zip', TRAIN_ZIP_IN, format='zip')
+    os.makedirs(TRAIN_CSV_OUT, exist_ok=True)
+    os.makedirs(TRAIN_IMG_OUT, exist_ok=True)
+    for file in os.listdir(TRAIN_CSV_IN):
+        process_annotations(os.path.join(TRAIN_CSV_IN, file), TRAIN_CSV_OUT)
+    for file in os.listdir(TRAIN_IMG_IN):
+        process_images(os.path.join(TRAIN_IMG_IN, file), TRAIN_IMG_OUT)
+    make_archive(TRAIN_ZIP_OUT, format='zip', root_dir=TRAIN_ZIP_OUT, base_dir='.')
 
-    shutil.make_archive('train-new', format='zip', root_dir='train', base_dir='.')
-    shutil.make_archive('test-new', format='zip', root_dir='test', base_dir='.')
+    unpack_archive(TEST_ZIP_IN + '.zip', TEST_ZIP_IN, format='zip')
+    os.makedirs(TEST_CSV_OUT, exist_ok=True)
+    os.makedirs(TEST_IMG_OUT, exist_ok=True)
+    for dir in os.listdir(TEST_CSV_IN):
+        for file in os.listdir(os.path.join(TEST_CSV_IN, dir)):
+            process_annotations(os.path.join(TEST_CSV_IN, dir, file), TEST_CSV_OUT)
+    for dir in os.listdir(TEST_IMG_IN):
+        # Do not process 'Annotations' dir
+        if dir == os.path.basename(TEST_CSV_IN):
+            continue
+        for file in os.listdir(os.path.join(TEST_IMG_IN, dir)):
+            process_images(os.path.join(TEST_IMG_IN, dir, file), TEST_IMG_OUT)
+    make_archive(TEST_ZIP_OUT, format='zip', root_dir=TEST_ZIP_OUT, base_dir='.')
