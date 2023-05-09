b34daf0b54505b0e3b3948c956c71f432dc045b2
The0Jan <The0Jan@users.noreply.github.com>
2023-05-09 16:26:26 +0200

    Working dataset and data loader

--- dataset.py
+++ dataset.py
@@ -1,15 +1,17 @@
 import os
-from torchvision.io import read_image
 from torch.utils.data import Dataset
 from torchvision.transforms import Resize, Compose, ToTensor, Normalize
 from torchvision.transforms import ToPILImage
+from PIL import Image
+
+def loader(path):
+    return Image.open(path).convert('RGB')
 
 class YOLODataset(Dataset):
     def __init__(
                 self,
                 annotations_dir, 
                 image_dir, 
-                image_size = 416,
                 transform=None, 
                 target_transform=None
                 ):
@@ -32,13 +34,11 @@ class YOLODataset(Dataset):
     def __getitem__(self, idx):
         img_path = os.path.join(self.img_dir, self.image_list[idx])
         anno_path = os.path.join(self.annotations_dir, self.annotations_list[idx])
-        
         bboxes = []
+        image = loader(img_path)
         
-        image = read_image(img_path)
         if self.transform:
             image = self.transform(image)
-        
         
         with open(anno_path, 'r') as anno_obj:
             csv_lines = anno_obj.readlines()
@@ -51,35 +51,35 @@ class YOLODataset(Dataset):
                     org_size = elements
                 else:
                     bboxes.append(elements)
-
-                
-
         if self.target_transform:
-            bboxes = self.target_transform(bboxes)
-            
+            bboxes = self.target_transform(bboxes)  
         return image, org_size, bboxes
     
 
+
+
 def tensor_to_image(tensor_image): 
-    to_pil_image = Compose([ ToPILImage()])
+    inv_imagenet_normalize = Normalize(mean=[-0.485/0.229, -0.456/0.224, -0.406/0.225], std=[1./0.229, 1./0.224, 1./0.225])
+    to_pil_image = Compose([inv_imagenet_normalize, ToPILImage()])
     image = to_pil_image(tensor_image)
     return image
 
 
 ###########################
-train_path_anno = 'test-new/Annotations'
-train_path_img =  'test-new/Images'
+check = False
+if check:
+    train_path_anno = 'test-new/annotations'
+    train_path_img =  'test-new/images'
 
+    image_size = (416, 416)
+    img_transform = Compose([Resize(image_size), ToTensor(), Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
 
-image_size = (416, 416)
-example_transform = Compose([Resize(image_size)])
+    yolo_dataset = YOLODataset(train_path_anno, train_path_img, transform=img_transform )
 
-
-yolo_dataset = YOLODataset(train_path_anno, train_path_img )
-
-
-tensor_image, org_size, label = yolo_dataset[1]
-image = tensor_to_image(tensor_image)
-image.show()
-print(org_size)
-print(label)
+    for i in range(10): 
+        print(yolo_dataset.annotations_list[i], yolo_dataset.image_list[i])
+    #tensor_image, org_size, label = yolo_dataset[8]
+    #image = tensor_to_image(tensor_image)
+    #image.show()
+    #print(org_size)
+    #print(label)
--- lightning_data.py
+++ lightning_data.py
@@ -1,40 +1,54 @@
 import torch
 import dataset
 import pytorch_lightning as pl
-from torch.utils.data import DataLoader, Subset
 from torchvision.transforms import Compose
+from torchvision.transforms import Resize, Compose, ToTensor, Normalize
+from torch.utils.data import DataLoader, random_split
 
 class MadaiModule(pl.LightningDataModule):
-
-    def __init__(self):
+    def __init__(self, batch_size = 32, 
+                 image_size = (416,416),
+                 train_anno_dir = 'train-new/annotations', 
+                 train_img_dir = 'train-new/images',
+                 test_anno_dir = 'test-new/annotations',
+                 test_img_dir = 'test-new/images',
+                 img_transform = None,
+                 target_transform = None):
         super().__init__()
-        self.batch_size = 16
+        self.batch_size     = batch_size
+        self.image_size     = image_size
+        self.train_anno_dir = train_anno_dir
+        self.train_img_dir  = train_img_dir
+        self.test_anno_dir  = test_anno_dir
+        self.test_img_dir   = test_img_dir
+        
+        self.img_transform  = img_transform
+        if img_transform is None:
+            self.img_transform = self.get_img_transform()
+        
+        self.target_transform   = target_transform
+        if target_transform is None:
+            self.target_transform = self.get_target_transform() 
 
     def prepare_data(self):
-        # download only
-        #torchvision.datasets.FashionMNIST(
-        #    root = './data/FashionMNIST',
-        #    train = True,
-        #    download = True,
-        #    transform = self.get_transform()
-        #)
-        #torchvision.datasets.FashionMNIST(
-        #    root = './data/FashionMNIST',
-        #    train = False,
-        #    download = True,
-        #    transform = self.get_transform()
-        #)
-        print("yes")
+        #pobieranie odpowiednich zipów 
+        return 0
 
     def setup(self, stage=None):
         # called on every GPU
         # use our dataset and defined transformations
-        dataset_yolo = dataset.YOLODataset('train-new/annotations', 'train-new/images')
-        indices = torch.randperm(len(dataset)).tolist()
-        self.dataset_train = Subset(dataset_yolo, indices[-10000:])
-        self.dataset_val = Subset(dataset_yolo, indices[-10000:])
-
-        self.dataset_test = dataset.YOLODataset('test-new/annotations', 'test-new/images')
+        if stage == 'fit' or stage is None:
+            dataset_yolo = dataset.YOLODataset(self.train_anno_dir, self.train_img_dir, 
+                                               transform = self.img_transform,
+                                               target_transform = self.target_transform)
+            
+            train_dataset_size = int(len(dataset_yolo) * 0.9)
+            self.dataset_train, self.dataset_val  = random_split(dataset_yolo, [train_dataset_size, len(dataset_yolo) - train_dataset_size])
+            
+        if stage == 'test' or stage is None:
+            self.dataset_test = dataset.YOLODataset(self.test_anno_dir, self.test_img_dir,
+                                               transform = self.img_transform,
+                                               target_transform = self.target_transform)
         
     def train_dataloader(self):
         return DataLoader(self.dataset_train, batch_size=self.batch_size, shuffle=True, pin_memory=True, num_workers=2)
@@ -45,5 +59,18 @@ class MadaiModule(pl.LightningDataModule):
     def val_dataloader(self):
         return DataLoader(self.dataset_val, batch_size=self.batch_size, shuffle=False, pin_memory=True, num_workers=2)
     
-    def get_transform(self):
-        return Compose([])
+    def get_img_transform(self):
+        return Compose([Resize(self.image_size), ToTensor(), Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+        
+    def get_target_transform(self):
+        return None
+    
+    
+if __name__ == "__main__":
+    dm = MadaiModule()
+    dm.setup()
+    
+    print(dm.dataset_train[0])
+    img = dataset.tensor_to_image(dm.dataset_train[0][0])
+    img.show()
+    dm.dataset_val
