cce9d19190a2187ba44621af049d3a83899e59ca
MortonPL <MortonPL@users.noreply.github.com>
2023-05-10 00:57:07 +0200

    temp

--- check.py
+++ check.py
@@ -4,23 +4,29 @@ from loading_weights import load_model_parameters
 import os
 from torchsummary import summary
 
+from torchvision.transforms import Resize, Compose, ToTensor, Normalize
+from dataset import YOLODataset
+from nms import after_party
 
 if __name__ == "__main__":
     WEIGHTS_DIR = 'weights'
     path_to_darknet = os.path.join(WEIGHTS_DIR, 'darknet53.conv.74')
     path_to_yolo = os.path.join(WEIGHTS_DIR, 'yolov3.weights')
 
-    model = darknet.Darknet53(3)
+    model = yolo.YOLOv3(80, 3)
     summary(model, input_size=(3,416,416), device='cpu')
-    #model = yolo.YOLOv3(80, 3)
-    #summary(model, input_size=(3,416,416), device='cpu')
-
-    for i, param in enumerate(model.named_parameters(), 0):
-        if i == 0:
-            print(param)
         
-    load_model_parameters(path_to_darknet, model)
+    load_model_parameters(path_to_yolo, model)
 
-    for i, param in enumerate(model.named_parameters(), 0):
-        if i == 0:
-            print(param)
+    train_path_anno = 'test-new/annotations'
+    train_path_img =  'test-new/images'
+    image_size = (416, 416)
+    img_transform = Compose([Resize(image_size), ToTensor(), Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+    yolo_dataset = YOLODataset(train_path_anno, train_path_img, transform=img_transform)
+
+    x, _, _, _ = yolo_dataset[0]
+    y = model(x)
+    print(y.shape)
+    z = after_party(y)
+    print(z.shape)
+    print(z)
