674fd36db8e5e3ce65a1b1cd3529877953087248
MortonPL <MortonPL@users.noreply.github.com>
2023-05-12 07:13:35 +0200

    Add PrimitiveDataset and PrimitiveDataModule for testing

--- check.py
+++ check.py
@@ -1,14 +1,14 @@
 import yolo
-import darknet
 from loading_weights import load_model_parameters
 import os
 from torchsummary import summary
 
-from torchvision.transforms import Resize, Compose, Lambda, ToTensor, Normalize
+from torchvision.transforms import Compose, Lambda, ToTensor, Normalize
 from dataset import YOLODataset
 from nms import after_party
 import dataset
 
+from primitive_dataloader import PrimitiveDataModule
 
 
 def get_img_transform():
@@ -26,14 +26,20 @@ if __name__ == "__main__":
 
     load_model_parameters(path_to_yolo, model)
 
-    train_path_anno = 'test-new/annotations'
-    train_path_img = 'test-val2017'
-    yolo_dataset = YOLODataset(train_path_anno, train_path_img, transform=get_img_transform(), target_transform=get_target_transform())
+    #train_path_anno = 'test-new/annotations'
+    #train_path_img = 'test-val2017'
+    #yolo_dataset = YOLODataset(train_path_anno, train_path_img, transform=get_img_transform(), target_transform=get_target_transform())
 
-    x, _, path, _ = yolo_dataset[4]
-    x = x.unsqueeze(0)
-    print(path)
-    y, loss = model(x, None)
-    z = after_party(y)
-    print(z)
-    dataset.visualize_results(path, z.tolist())
+    data_model = PrimitiveDataModule(None, 'test-val2017', batch_size=1, num_workers=4, img_transform=get_img_transform())
+    data_model.setup()
+
+    for i, batch in enumerate(data_model.predict_dataloader()):
+        if i == 0:
+            x, _, path, _ = batch
+            x, path = x[0], path[0]
+            x = x.unsqueeze(0)
+            print(path)
+            y, loss = model(x, None)
+            z = after_party(y)
+            print(z)
+            dataset.visualize_results(path, z.tolist())
