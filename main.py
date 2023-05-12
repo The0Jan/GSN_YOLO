bf094293e5b5f9c37a743faec75dd998952bdc6f
MortonPL <MortonPL@users.noreply.github.com>
2023-05-12 23:17:57 +0200

    Add direct checkpoint downloading

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ import argparse
 import os
 import gdown
 
+CHECKPOINT_GID_BEST = "16x4pcp_mWr-MSv0TcliAGyfk843EjXWp"
+CHECKPOINT_GID_SECOND = "1IlvbUfkeNNjITpWeG3IsJPJSrg2nPEhf"
 
 def predict(model, datamodule, output, batch_count):
     os.makedirs(output, exist_ok=True)
@@ -30,6 +32,7 @@ def parse_args():
     parser.add_argument('-b','--batch_count', type=int, default=1, help='Number of batches to work on')
     parser.add_argument('-c','--checkpoint', type=str, default=None, help='Load model checkpoint from file')
     parser.add_argument('-e','--earlystop', type=bool, default=True, help='Should use early stop callback?')
+    parser.add_argument('-g','--checkpoint_gid', type=str, default=None, help='Load model checkpoint from Google Drive')
     parser.add_argument('-i','--input', type=str, default='inputs', help='Prediction inputs directory')
     parser.add_argument('-l', '--logger', type=str, choices=['csv', 'tensorboard', 'wandb'], default='csv', help='Logger to use')
     parser.add_argument('-o','--output', type=str, default='predictions', help='Prediction outputs directory')
@@ -37,7 +40,7 @@ def parse_args():
     return parser.parse_args()
 
 
-def download(weights_dir: str, model_dir: str):
+def download(weights_dir: str, model_dir: str, model_gid: str):
     # Download pretrained weights
     weights_gid = "1mL2BC_UM3iHXMZFmKgrlO9jP5yVdYMbr"
     weights_file = "darknet53.conv.74"
@@ -47,14 +50,14 @@ def download(weights_dir: str, model_dir: str):
     if not os.path.isfile(weights_file):
         gdown.download(id=weights_gid, output=weights_file)
     # Download best model checkpoint
-    model_gid = "16x4pcp_mWr-MSv0TcliAGyfk843EjXWp"
-    model_dir = "model"
-    model_file = "best.ckpt"
-    if not os.path.isdir(model_dir):
-        os.mkdir(model_dir)
-    model_file = os.path.join(model_dir, model_file)
-    if not os.path.isfile(model_file):
-        gdown.download(id=model_gid, output=model_file)
+    if model_gid is not None:
+        model_dir = "model"
+        model_file = model_gid + ".ckpt"
+        if not os.path.isdir(model_dir):
+            os.mkdir(model_dir)
+        model_file = os.path.join(model_dir, model_file)
+        if not os.path.isfile(model_file):
+            gdown.download(id=model_gid, output=model_file)
 
 
 def main(args):
@@ -74,7 +77,7 @@ def main(args):
         mode='min'
     )
     # Download files
-    download(weights_dir="weights", model_dir=MODEL_CKPT_PATH)
+    download(weights_dir="weights", model_dir=MODEL_CKPT_PATH, model_gid=args.checkpoint_gid)
     # Choose logger
     logger = None
     if args.logger == 'csv':
@@ -86,6 +89,8 @@ def main(args):
     # Choose model or model checkpoint
     if args.checkpoint is not None:
         yolo_model = YOLOv3Module.load_from_checkpoint(MODEL_CKPT_PATH + args.checkpoint)
+    elif args.checkpoint_gid is not None:
+        yolo_model = YOLOv3Module.load_from_checkpoint(MODEL_CKPT_PATH + args.checkpoint_gid + ".ckpt")
     else:
         yolo_model = YOLOv3Module(num_classes=5)
     # Set up callbacks
